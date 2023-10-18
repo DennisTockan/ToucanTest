2759b6e17fd972d78c867130efe29526df5c4fd2
Dennis <dennis@Denniss-MacBook-Air.local>
2023-10-18 14:18:27 +0100

    updated project

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,10 +10,50 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_home_page_successful(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/home');
 
         $response->assertStatus(200);
     }
+
+    public function test_create_page_successful(): void
+    {
+        $response = $this->get('/create');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_homeid_page_successful(): void
+    {
+         $school_id = 1;
+         $response = $this->get("/home/{$school_id}");
+
+        $response->assertStatus(200);
+    }
+
+    public function test_create_page_unsuccessful(): void
+    {
+        $response = $this->get('/notfound');
+
+        $response->assertStatus(404);
+    }
+
+    public function test_post_create_page_successful(): void
+    {
+        $data = [
+            'name' => 'John Doe',
+            'email' => 'johndoe@example.com',
+            'school' => '1'
+        ];
+    
+        $response = $this->post(route('store.member'), $data);
+
+        // Check that a new member has been added to the database
+        $this->assertDatabaseHas('members', ['name' => 'John Doe', 'email' => 'johndoe@example.com', 'school_id' => '1']);
+    
+        // Check that the user is redirected to the expected route
+        $response->assertRedirect('/home');
+    }
+    
 }
